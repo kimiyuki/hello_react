df67ad16e072ff173d65482cd4754172dc3fd8eb
kimiyuki shirai <kimiyuki@gmail.com>
2019-03-27 13:03:28 +0900

    class components and fucntional component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1> Hello World</h1>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+//class App extends Component {
+//  render() {
+//    return (
+//      <React.Fragment>
+//      <h1>hello</h1>
+//      <label htmlFor='bar'>bar</label>
+//      <input type='text' onChange={() => {console.log("I am clicked")}} />
+//      </React.Fragment>
+//      )
+//  }
+//}
+
+const App = () => {
+  return (
+    <div>
+      <Cat />  
+      <Cat />  
+      <Cat />  
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
